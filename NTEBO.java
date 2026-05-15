e559b444e367d618f4ef821fa1eba02472d799f1
25000298 <25000298@mvula.univen.ac.za>
2026-05-15 10:52:25 +0200

    Add files via upload

---
+++ NTEBO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.display;
+
+/**
+ *
+ * @author 25000298
+ */
+public class Display {
+
+    public static void main(String[] args) {
+String name="Ntebo";
+System.out.println(name);
+    }
+        
+        
+}
